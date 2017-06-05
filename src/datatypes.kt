8d6ea9a3622938f15a7eb4156e7cdcc91717b9e0
BrunoRov <brunorov@jitenshas.com>
2017-06-04 19:02:17 -0500

    primer commit

---
+++ src/datatypes.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by bruno on 4/06/17.
+ */
+
+fun main(args:Array<String>){
+    var x =   10
+    var y = 12
+    var name = "Bruno"
+    var dep:String
+    dep= "Software"
+    println("Name:"+name)
+    println("Department:"+dep)
+
+}
